cc3776ad7dbee17a61bcf097571a9aab15d644cc
Hulk <2279177509@qq.com>
2022-07-04 23:12:33 +0800

    20220704-2311, [Test] add solve for floating base dynamics.

--- pinocchio_test.py
+++ pinocchio_test.py
@@ -10,6 +10,8 @@ import pybullet_data
 import numpy as np
 import pinocchio as pin
 
+import casadi as ca
+
 
 class Planner:
     def __init__(self):
@@ -69,8 +71,8 @@ if __name__ == '__main__':
     # dq = np.matrix(np.random.rand(model.nv, 1))
     # ddq = np.matrix(np.random.rand(model.nv, 1))
     q = np.matrix([0.5]*model.nq).T
-    dq = np.matrix([1]*model.nv).T
-    ddq = np.matrix([1]*model.nv).T
+    dq = np.matrix([1.0]*model.nv).T
+    ddq = np.matrix([1.0]*model.nv).T
     tau = pin.rnea(model, data, q, dq, ddq)
     # print('tau = ', tau.T)
 
@@ -110,3 +112,68 @@ if __name__ == '__main__':
     f = np.matrix([0.4, 0.5, 6]*4).T
     tau = M * ddq + nle - J.T*f
     print('tau = ', tau.T)
+
+    # x = tau-qdd_delta-force_delta
+    # H = ca.DM.eye(18)
+    # for i in range(6, 18):
+    #     H[i, i] = 0
+    # # A = ca.DM.eye(18)
+    # g = ca.DM.zeros(18)
+    # lbx = M * ddq + nle - J.T*f
+    # ubx = M * ddq + nle - J.T*f
+    # qp = {'h': H.sparsity()}
+    # S = ca.conic('S', 'qpoases', qp)
+    # r = S(h=H, g=g, lbx=lbx, ubx=ubx)
+    # x_opt = r['x']
+
+    # tau + J.T*f = M * ddq + nle
+    # x = tau(18)-qdd_delta(6)-force_delta(12)
+    H = ca.DM.eye(6 + 12)
+    # for i in range(18):
+    #     H[i, i] = 0
+    g = ca.DM.zeros(6 + 12)
+    # A = ca.DM.eye(18)
+    A = ca.DM.zeros(6, 6 + 12)
+    A[0:6, 0:6] = -M[0:6, 0:6]
+    A[0:6, 6:18] = J.T[0:6, :]
+    lba = ca.DM.zeros(6)
+    lba[0:6] = (M * ddq + nle - J.T * f)[0:6, :]
+    uba = lba
+    qp = {'h': H.sparsity(), 'a': A.sparsity()}
+    S = ca.conic('S', 'qpoases', qp)
+    r = S(h=H, g=g, a=A, lba=lba, uba=uba)
+    x_opt = r['x']
+    print('x_opt:', x_opt)
+    ddq[0:6, 0] = ddq[0:6, 0] + x_opt[0:6]
+    f[0:12, 0] = f[0:12, 0] + x_opt[6:18]
+    print('M * ddq + nle - J.T*f:', M * ddq + nle - J.T * f)
+
+    # tau + J.T*f = M * ddq + nle
+    # x = tau(18)-qdd_delta(6)-force_delta(12)
+    H = ca.DM.eye(18+6+12)
+    for i in range(18):
+        H[i, i] = 0
+    g = ca.DM.zeros(18+6+12)
+    # A = ca.DM.eye(18)
+    A = ca.DM.zeros(24, 36)
+    A[0:18, 0:18] = ca.DM.eye(18)
+    A[0:18, 18:24] = -M[0:18, 0:6]
+    A[0:18, 24:36] = J.T
+    for i in range(6):
+        A[18+i, i] = 1
+    lba = ca.DM.zeros(24)
+    lba[0:18] = M * ddq + nle - J.T*f
+    uba = lba
+    qp = {'h': H.sparsity(), 'a': A.sparsity()}
+    S = ca.conic('S', 'qpoases', qp)
+    r = S(h=H, g=g, a=A, lba=lba, uba=uba)
+    x_opt = r['x']
+    print('x_opt:', x_opt)
+    ddq[0:6, 0] = ddq[0:6, 0] + x_opt[18:24]
+    f[0:12, 0] = f[0:12, 0] + x_opt[24:36]
+    print('M * ddq + nle - J.T*f:', M * ddq + nle - J.T*f)
+
+    # a = np.array([0, 1.0])
+    # a[0] = 0.1
+    # print(a)
+
