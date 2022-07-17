9065e2b88496938b5afda6bb4b769e3705bdd326
Hulk <2279177509@qq.com>
2022-07-17 23:49:01 +0800

    20220717-2343, [Ctrl] stablize stand motion.
    adjust joint order to be consitent with pybullet;
    add rotation control;
    add frction cone constrain in optimization problem;
    add options in casadi solver.

--- quadruped_controller.py
+++ quadruped_controller.py
@@ -11,6 +11,7 @@ import numpy as np
 import pinocchio as pin
 import casadi as ca
 
+from scipy.spatial.transform import Rotation as R
 
 class Estimation:
     def __init__(self):
@@ -77,8 +78,11 @@ if __name__ == '__main__':
     # model = pin.buildModelFromUrdf(pybullet_data.getDataPath()+'/urdf/quadruped_robot/quadruped_robot.urdf')
     data = model.createData()
 
-    body_pos_des = np.asarray([0, 0, 0.16, 0, 0, 0])
-    body_pos_fdb = np.asarray(env.reset()[0:6])
+    body_pos_des = np.asmatrix([0, 0, 0.14])
+    body_pos_fdb = np.asmatrix(env.reset()[0:3])
+    body_rot_des = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    body_rot_fdb = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    body_acc_des = np.asmatrix([0] * 6).T
 
     torque = [.0] * 12
     for i in range(100000):
@@ -95,17 +99,27 @@ if __name__ == '__main__':
         # foot_force_cmd = np.linalg.pinv(Mfb)*body_force_cmd
         # body_acc_cmd = body_force_cmd/3
 
+        # simulation
+        torque = list(torque[0:3])+ list(torque[6:9])+ list(torque[3:6])+ list(torque[9:12])
         [o, pb, vb, js] = env.step_torque(torque)
         # print("pb:", pb)
         # print("vb:", vb)
         # print("js:", js)
+        js = list(js[0:3]) + list(js[6:9]) + list(js[3:6]) + list(js[9:12])
+
         pj = [i[0] for i in js]
         vj = [i[1] for i in js]
         q = np.asmatrix(list(pb[0] + pb[1]) + pj).T
         v = np.asmatrix(list(vb[0] + vb[1]) + vj).T
 
         body_pos_fdb[0:3] = pb[0]
-        body_acc_des = np.asmatrix(10 * (body_pos_des - body_pos_fdb)).T
+        body_rot_fdb = R.from_quat(pb[1])
+
+        body_acc_des[0:3, 0] = np.asmatrix(80 * (body_pos_des - body_pos_fdb)).T
+        body_acc_des[3:6, 0] = np.asmatrix(80 * (body_rot_des * body_rot_fdb.inv()).as_rotvec()).T
+
+        # body_acc_des = body_acc_des -8*np.asmatrix(vb[0]+vb[1]).T
+
         p = np.asmatrix([0] * 12).T
         print("torque:", torque)
         print("body_pos_fdb:", body_pos_fdb)
@@ -177,25 +191,42 @@ if __name__ == '__main__':
         tor = np.matrix([.0, .0, .0] * 6).T
 
         # tau + J.T*f = M * ddq + nle
-        # x = tau(18)-force_delta(12)
+        # x = tau(18)-force(12)
         H = ca.DM.eye(18 + 12)
         # for i in range(18):
         #     H[i, i] = 0
         g = ca.DM.zeros(18 + 12)
+
         # A = ca.DM.eye(18)
-        A = ca.DM.zeros(24, 30)
+        A = ca.DM.zeros(34, 30)
         A[0:18, 0:18] = ca.DM.eye(18)
         A[0:18, 18:30] = JF.T
+        mu_c = 0.3
+        # friction cone constrain
+        A[18:22, 18:21] = np.asmatrix([[1, 0, -mu_c], [-1, 0, -mu_c], [0, 1, -mu_c], [0, -1, -mu_c]])
+        A[22:26, 21:24] = np.asmatrix([[1, 0, -mu_c], [-1, 0, -mu_c], [0, 1, -mu_c], [0, -1, -mu_c]])
+        A[26:30, 24:27] = np.asmatrix([[1, 0, -mu_c], [-1, 0, -mu_c], [0, 1, -mu_c], [0, -1, -mu_c]])
+        A[30:34, 27:30] = np.asmatrix([[1, 0, -mu_c], [-1, 0, -mu_c], [0, 1, -mu_c], [0, -1, -mu_c]])
         # A[0:18, 18:30] = JF.T.zeros()
         # A[0:18, 18:30] = ca.DM.zeros(18, 12)
-        for i in range(6):
-            A[18 + i, i] = 1
-        lba = ca.DM.zeros(24)
+        lba = ca.DM.zeros(34)
         lba[0:18] = M * ddq + nle
-        uba = lba
+        lba[18:34] = -1000
+
+        uba = ca.DM(lba)
+        uba[18:34] = 0
+        ubx = ca.DM.zeros(30)
+        ubx[0:6] = 0.000001
+        ubx[6:18] = 20
+        ubx[18:30] = 200
+        lbx = ca.DM(-ubx)
+        lbx[20:30:3] = 0
+
         qp = {'h': H.sparsity(), 'a': A.sparsity()}
-        S = ca.conic('S', 'qpoases', qp)
-        r = S(h=H, g=g, a=A, lba=lba, uba=uba)
+        opts = {'printLevel': 'none'}
+        # opts = {'printLevel': 'none', 'error_on_fail': False}
+        S = ca.conic('S', 'qpoases', qp, opts)
+        r = S(h=H, g=g, a=A, lba=lba, uba=uba, lbx=lbx, ubx=ubx)
         x_opt = r['x']
         # print('x_opt:', x_opt)
 
