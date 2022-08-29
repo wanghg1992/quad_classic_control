3751d6b3bf21f9b6880e72ded56e777552144d66
Hulk <2279177509@qq.com>
2022-08-29 21:39:35 +0800

    20220829-2139, [Ctrl] normalize module interface.

--- quadruped_controller.py
+++ quadruped_controller.py
@@ -13,27 +13,31 @@ import casadi as ca
 
 from scipy.spatial.transform import Rotation as R
 
+import math
+
 class Estimation:
     def __init__(self):
-        self.pb = np.asarray([0] * 12)
+        self.q_ = np.asarray([0] * 19)
+        self.pb_ = np.asarray([0, 0, 0.16, 0., 0., 0., 1.])
+        self.pf_ = np.asarray([0] * 12)
+        self.vb_ = np.asarray([0, 0, 0, 0., 0., 0.])
 
     def step(self):
-        self.pb[2] = 0.12
-        return self.pb
-
+        return self.pb_
 
 class Planner:
     def __init__(self):
-        self.pb = np.asarray([0] * 12)
-
-    def step(self):
-        self.pb[2] = 0.12
-        return self.pb
-
+        self.pb = np.asarray([0, 0, 0.16, 0., 0., 0., 1.])
+        self.vb = np.asarray([0] * 6)
 
 class Controller:
     def __init__(self):
         self.torque = np.asarray([0] * 12)
+        self.body_pos_des = np.asmatrix([0, 0, 0.16])
+        self.body_rot_des = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        self.body_acc_des = np.asmatrix([0] * 6).T
+        self.body_pos_fdb = np.asmatrix(env.reset()[0:3])
+        self.body_rot_fdb = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
     def step_high_slope(self, p, o):
         kp = np.asarray([100] * 6)
@@ -64,129 +68,82 @@ class Controller:
             #     action[leg * 3 + 2] = -0.05*2
         return action
 
-    # def
+    def optimal_control(self, est, plan):
+        q_ = est.q_
+        dq_ = est.dq_
 
+        self.body_pos_des = plan.pb[0:3]
+        self.body_rot_des = R.from_quat(plan.pb[3:7])
+        self.body_pos_fdb = est.pb_[0:3]
+        self.body_rot_fdb = R.from_quat(est.pb_[3:7])
 
-if __name__ == '__main__':
-    id = "gym_env:Quadruped-v0"
-    env = gym.make(id)
+        self.body_acc_des[0:3, 0] = np.asmatrix(80 * (self.body_pos_des - self.body_pos_fdb)).T
+        self.body_acc_des[3:6, 0] = np.asmatrix(80 * (self.body_rot_des * self.body_rot_fdb.inv()).as_rotvec()).T
 
-    ## quadruped model
-    root = pin.JointModelFreeFlyer()
-    model = pin.buildModelFromUrdf(pybullet_data.getDataPath() + '/urdf/quadruped_robot/quadruped_robot.urdf',
-                                   root)
-    # model = pin.buildModelFromUrdf(pybullet_data.getDataPath()+'/urdf/quadruped_robot/quadruped_robot.urdf')
-    data = model.createData()
+        self.body_acc_des = self.body_acc_des - 8 * np.asmatrix(est.vb_).T
 
-    body_pos_des = np.asmatrix([0, 0, 0.14])
-    body_pos_fdb = np.asmatrix(env.reset()[0:3])
-    body_rot_des = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    body_rot_fdb = R.from_matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    body_acc_des = np.asmatrix([0] * 6).T
-
-    torque = [.0] * 12
-    for i in range(100000):
-        # body_force = Mfb*foot_fore
-
-        # Mfb0 = np.append(np.array(np.eye(3)),pin.skew(p[0:3]),axis=0)
-        # Mfb1 = np.append(np.array(np.eye(3)),pin.skew(p[3:6]),axis=0)
-        # Mfb2 = np.append(np.array(np.eye(3)),pin.skew(p[6:9]),axis=0)
-        # Mfb3 = np.append(np.array(np.eye(3)),pin.skew(p[9:12]),axis=0)
-        # Mfb = np.append(Mfb0,Mfb1,axis=1)
-        # Mfb = np.append(Mfb,Mfb2,axis=1)
-        # Mfb = np.append(Mfb,Mfb3,axis=1)
-        #
-        # foot_force_cmd = np.linalg.pinv(Mfb)*body_force_cmd
-        # body_acc_cmd = body_force_cmd/3
-
-        # simulation
-        torque = list(torque[0:3])+ list(torque[6:9])+ list(torque[3:6])+ list(torque[9:12])
-        [o, pb, vb, js] = env.step_torque(torque)
-        # print("pb:", pb)
-        # print("vb:", vb)
-        # print("js:", js)
-        js = list(js[0:3]) + list(js[6:9]) + list(js[3:6]) + list(js[9:12])
-
-        pj = [i[0] for i in js]
-        vj = [i[1] for i in js]
-        q = np.asmatrix(list(pb[0] + pb[1]) + pj).T
-        v = np.asmatrix(list(vb[0] + vb[1]) + vj).T
-
-        body_pos_fdb[0:3] = pb[0]
-        body_rot_fdb = R.from_quat(pb[1])
-
-        body_acc_des[0:3, 0] = np.asmatrix(80 * (body_pos_des - body_pos_fdb)).T
-        body_acc_des[3:6, 0] = np.asmatrix(80 * (body_rot_des * body_rot_fdb.inv()).as_rotvec()).T
-
-        # body_acc_des = body_acc_des -8*np.asmatrix(vb[0]+vb[1]).T
-
-        p = np.asmatrix([0] * 12).T
-        print("torque:", torque)
-        print("body_pos_fdb:", body_pos_fdb)
-
-        # q[3] = pb[1][3]
-        # q[4] = pb[1][0]
-        # q[5] = pb[1][1]
-        # q[6] = pb[1][2]
-        # q[3] = 1
-        # q[4] = 0
-        # q[5] = 0
-        # q[6] = 0
-        pin.forwardKinematics(model, data, q)
+        pin.forwardKinematics(model, data, q_)
         pin.updateFramePlacements(model, data)
         # for frame, oMf in zip(model.frames, data.oMf):
         #     print(("{:<24} : {: .2f} {: .2f} {: .2f}"
         #            .format(frame.name, *oMf.translation.T.flat)))
-        pin.computeJointJacobians(model, data, q)
-        Jrf = np.matrix(pin.getFrameJacobian(model, data, model.getFrameId('RF4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
-        Jlf = np.matrix(pin.getFrameJacobian(model, data, model.getFrameId('LF4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
-        Jrh = np.matrix(pin.getFrameJacobian(model, data, model.getFrameId('RH4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
-        Jlh = np.matrix(pin.getFrameJacobian(model, data, model.getFrameId('LH4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        pin.computeJointJacobians(model, data, q_)
+        Jrf = np.matrix(
+            pin.getFrameJacobian(model, data, model.getFrameId('RF4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        Jlf = np.matrix(
+            pin.getFrameJacobian(model, data, model.getFrameId('LF4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        Jrh = np.matrix(
+            pin.getFrameJacobian(model, data, model.getFrameId('RH4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        Jlh = np.matrix(
+            pin.getFrameJacobian(model, data, model.getFrameId('LH4_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         Jfoot = np.append(Jrf[0:3, :], Jlf[0:3, :], axis=0)
         Jfoot = np.append(Jfoot, Jrh[0:3, :], axis=0)
         Jfoot = np.append(Jfoot, Jlh[0:3, :], axis=0)
 
-        pin.computeJointJacobiansTimeVariation(model, data, q, v)
+        pin.computeJointJacobiansTimeVariation(model, data, q_, dq_)
         Jdrf = np.matrix(pin.getFrameJacobianTimeVariation(model, data, model.getFrameId('RF4_joint'),
-                                                 pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+                                                           pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         Jdlf = np.matrix(pin.getFrameJacobianTimeVariation(model, data, model.getFrameId('LF4_joint'),
-                                                 pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+                                                           pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         Jdrh = np.matrix(pin.getFrameJacobianTimeVariation(model, data, model.getFrameId('RH4_joint'),
-                                                 pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+                                                           pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         Jdlh = np.matrix(pin.getFrameJacobianTimeVariation(model, data, model.getFrameId('LH4_joint'),
-                                                 pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+                                                           pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         Jdfoot = np.append(Jdrf[0:3, :], Jdlf[0:3, :], axis=0)
         Jdfoot = np.append(Jdfoot, Jdrh[0:3, :], axis=0)
         Jdfoot = np.append(Jdfoot, Jdlh[0:3, :], axis=0)
 
-        pin.crba(model, data, q)
+        pin.crba(model, data, q_)
         M = np.asmatrix(data.M)
         ddx = np.zeros(12).reshape(12, 1)
         JF = Jfoot
-        JF_pinv = np.linalg.inv(M)*JF.T*np.linalg.inv(JF*np.linalg.inv(M)*JF.T)
-        ddq = JF_pinv * (ddx - Jdfoot * v)
-        N1 = np.eye(18) - JF_pinv*JF
+        JF_pinv = np.linalg.inv(M) * JF.T * np.linalg.inv(JF * np.linalg.inv(M) * JF.T)
+        ddq = JF_pinv * (ddx - Jdfoot * dq_)
+        N1 = np.eye(18) - JF_pinv * JF
 
-        JB = np.matrix(pin.getFrameJacobian(model, data, model.getFrameId('root_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        JB = np.matrix(
+            pin.getFrameJacobian(model, data, model.getFrameId('root_joint'), pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
         JdB = np.matrix(pin.getFrameJacobianTimeVariation(model, data, model.getFrameId('root_joint'),
-                                                 pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
-        JB_pinv = np.linalg.inv(M)*JB.T*np.linalg.inv(JB*np.linalg.inv(M)*JB.T)
+                                                          pin.ReferenceFrame.LOCAL_WORLD_ALIGNED))
+        JB_pinv = np.linalg.inv(M) * JB.T * np.linalg.inv(JB * np.linalg.inv(M) * JB.T)
         # N1 = np.eye(18) - JB_pinv*JB
         J2 = JB
         Jd2 = JdB
-        J2_pre = J2*N1
-        J2_pre_dpinv = np.linalg.inv(M)*J2_pre.T*np.linalg.inv(J2_pre*np.linalg.inv(M)*J2_pre.T)
-        ddx = body_acc_des
-        ddq = ddq + J2_pre_dpinv * (ddx - Jd2 * v - J2 * ddq)
+        J2_pre = J2 * N1
+        J2_pre_dpinv = np.linalg.inv(M) * J2_pre.T * np.linalg.inv(J2_pre * np.linalg.inv(M) * J2_pre.T)
+        ddx = self.body_acc_des
+        ddq = ddq + J2_pre_dpinv * (ddx - Jd2 * dq_ - J2 * ddq)
 
-        pin.forwardKinematics(model, data, q, v, ddq)
-        a = pin.getFrameAcceleration(model, data,model.getFrameId('LFFoot_link'),pin.LOCAL_WORLD_ALIGNED)
+        pin.forwardKinematics(model, data, q_, dq_, ddq)
+        a = pin.getFrameAcceleration(model, data, model.getFrameId('LFFoot_link'), pin.LOCAL_WORLD_ALIGNED)
+        # a = pin.getFrameAcceleration(model, data,model.getFrameId('LFFoot_link'), pin.LOCAL)
         # print("a LFFoot: ", a)
-        a = pin.getFrameAcceleration(model, data,model.getFrameId('body_link'),pin.LOCAL_WORLD_ALIGNED)
+        a = pin.getFrameAcceleration(model, data, model.getFrameId('body_link'), pin.LOCAL_WORLD_ALIGNED)
+        # a = pin.getFrameAcceleration(model, data,model.getFrameId('body_link'), pin.LOCAL)
         # print("a body: ", a)
 
-        M = pin.crba(model, data, q)
-        nle = np.matrix(pin.nle(model, data, q, v)).T
+        M = pin.crba(model, data, q_)
+        nle = np.matrix(pin.nle(model, data, q_, dq_)).T
         f = np.matrix([.0, .0, .0] * 4).T
         tor = np.matrix([.0, .0, .0] * 6).T
 
@@ -223,8 +180,8 @@ if __name__ == '__main__':
         lbx[20:30:3] = 0
 
         qp = {'h': H.sparsity(), 'a': A.sparsity()}
-        opts = {'printLevel': 'none'}
-        # opts = {'printLevel': 'none', 'error_on_fail': False}
+        # opts = {'printLevel': 'none'}
+        opts = {'printLevel': 'none', 'error_on_fail': False}
         S = ca.conic('S', 'qpoases', qp, opts)
         r = S(h=H, g=g, a=A, lba=lba, uba=uba, lbx=lbx, ubx=ubx)
         x_opt = r['x']
@@ -233,12 +190,57 @@ if __name__ == '__main__':
         tor[0:18, 0] = x_opt[0:18]
         f[0:12, 0] = x_opt[18:30]
         # print('M * ddq + nle - J.T*f:', M * ddq + nle - JF.T * f)
+        # print('ddq:', np.linalg.inv(M) * (tor + JF.T * f - nle))
 
         torque = tor[6:18]
 
-        pin.aba(model, data, q, v, tor)
+        pin.aba(model, data, q_, dq_, tor)
         a_f = data.ddq
-        print('a_f:', a_f)
+        # print('a_f:', a_f)
+
+        return torque
+
+    def step(self, est, plan):
+        return self.optimal_control(est, plan)
+
+
+
+if __name__ == '__main__':
+    id = "gym_env:Quadruped-v0"
+    env = gym.make(id)
+
+    ## quadruped model
+    root = pin.JointModelFreeFlyer()
+    model = pin.buildModelFromUrdf(pybullet_data.getDataPath() + '/urdf/quadruped_robot/quadruped_robot.urdf',
+                                   root)
+    # model = pin.buildModelFromUrdf(pybullet_data.getDataPath()+'/urdf/quadruped_robot/quadruped_robot.urdf')
+    data = model.createData()
+
+    torque = [.0] * 12
+
+    est = Estimation()
+    plan = Planner()
+    control = Controller()
+    for i in range(100000):
+
+        # simulation
+        torque = list(torque[0:3])+ list(torque[6:9])+ list(torque[3:6])+ list(torque[9:12])
+        [o_, pb_, vb_, js_] = env.step_torque(torque)
+        js_ = list(js_[0:3]) + list(js_[6:9]) + list(js_[3:6]) + list(js_[9:12])
+
+        pj_ = [i[0] for i in js_]
+        vj_ = [i[1] for i in js_]
+        q_ = np.asmatrix(list(pb_[0] + pb_[1]) + pj_).T
+        dq_ = np.asmatrix(list(vb_[0] + vb_[1]) + vj_).T
+
+        est.q_ = q_
+        est.dq_ = dq_
+        est.pb_[0:3] = pb_[0]
+        est.pb_[3:7] = pb_[1]
+        est.vb_[0:3] = vb_[0]
+        est.vb_[3:6] = vb_[1]
+
+        torque = control.step(est, plan)
 
         # env.render()
     input("press any key to continue...")
